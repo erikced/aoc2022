7de04e9ab03d352b5a4da89d902aded8905c4e3f
Erik Cederberg <erikced@gmail.com>
2022-12-06 19:07:31 +0100

    Update day 6

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
--- src/day06.rs
+++ src/day06.rs
@@ -1,45 +1,46 @@
 use std::collections::VecDeque;
-use std::collections::BTreeSet;
-use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::cmp::min;
 
 static FILENAME: &str = "data/day06";
 
 pub fn task1() {
-    let pos = load_data().chars().tuple_windows().enumerate().filter_map(|(pos, (a, b, c, d))| {
-        if a != b && a != c && a != d && b != c && b != d && c != d {
-            Some(pos + 4) 
-        } else {
-            None
-        }
-    }).nth(0).unwrap();
     println!("Day 6, task 1");
-    println!("Position: {}", pos);
+    println!("Position: {}", get_start_position(4));
 }
-
-const N_MSG_CHARS: usize = 14;
 
 pub fn task2() {
-    let mut buffer = VecDeque::<char>::new();
-    let pos = load_data().chars().enumerate().filter_map(|(idx, ch)| {
-        buffer.push_back(ch);
-        if buffer.len() > N_MSG_CHARS {
-            buffer.pop_front();
-        }
-        if buffer.iter().copied().collect::<BTreeSet<_>>().len() == 14 {
-            Some(idx + 1) 
-        } else {
-            None
-        }
-    }).nth(0).unwrap();
     println!("Day 6, task 2");
-    println!("Position: {}", pos);
+    println!("Position: {}", get_start_position(14));
 }
 
-fn load_data() -> String {
+fn get_start_position(num_unique: usize) -> usize {
     let mut reader = BufReader::new(File::open(FILENAME).unwrap());
     let mut line = String::new();
     reader.read_line(&mut line).unwrap();
-    return line
+    let mut buffer = VecDeque::<char>::new();
+    let mut duplicate_buffer = Vec::<char>::new();
+    line.chars()
+        .enumerate()
+        .filter_map(|(idx, ch)| {
+            buffer.push_front(ch);
+            buffer.resize_with(min(buffer.len(), num_unique), || unreachable!());
+            duplicate_buffer.clear();
+            let num_unique_found = buffer.iter().map(|ch| {
+                if duplicate_buffer.contains(ch) {
+                    false
+                } else {
+                    duplicate_buffer.push(*ch);
+                    true
+                }
+            }).take_while(|x| *x).count();
+            if num_unique_found == num_unique {
+                Some(idx + 1)
+            } else {
+                None
+            }
+        })
+        .nth(0)
+        .unwrap()
 }
